2466750bd9850cf04a21d4b66a47bb4c569bbe58
IgorMontezuma20 <igorminranda17@gmail.com>
2024-12-22 20:29:27 -0300

    feat: Add message skeleton component with messages state

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -11,7 +11,13 @@ const ChatContainer = () => {
     getMessages(selectedUser?._id);
   }, [selectedUser, getMessages]);
 
-  if (isMessagesLoading) return <MessageSkeleton />;
+  if (isMessagesLoading) return(
+    <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput/>
+    </div>
+  );
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
