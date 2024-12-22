55afbd26f6fb861616ada92e4ec579cc3821d079
IgorMontezuma20 <igorminranda17@gmail.com>
2024-12-22 20:25:11 -0300

    feat: Create chat header component

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+
 const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+  useEffect(() => {
+    getMessages(selectedUser?._id);
+  }, [selectedUser, getMessages]);
+
+  if (isMessagesLoading) return <MessageSkeleton />;
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>Mensagens...</p>
+      <MessageInput/>
+    </div>
+  );
 };
 
 export default ChatContainer;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import { Users } from "lucide-react";
 import { useEffect } from "react";
+import { useAuthStore } from "../store/useAuthStore";
 import { useChatStore } from "../store/useChatStore";
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 
 const Sidebar = () => {
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } =
     useChatStore();
-  const onlineUsers = [];
+
+  const { onlineUsers } = useAuthStore();
 
   useEffect(() => {
     getUsers();
